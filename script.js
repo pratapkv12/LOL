68e41442569f6c5d9e0f75da6fc54f066b158b14
pratapkv12 <160513378+pratapkv12@users.noreply.github.com>
2024-09-14 15:29:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        alert('More details about this cake will be available soon!');
+    });
+});
